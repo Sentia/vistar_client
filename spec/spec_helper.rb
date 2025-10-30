0bee2ab829b30ce51513a6c3094ff289757438aa
Chayut Orapinpatipat <chayut@canopusnet.com>
2025-10-31 10:29:32 +1100

    day1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
-require "vistar_client"
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/vendor/'
+  minimum_coverage 95
+  minimum_coverage_by_file 80
+end
+
+require 'vistar_client'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
@@ -12,4 +21,11 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  # Run specs in random order
+  config.order = :random
+  Kernel.srand config.seed
 end
+
+# Load support files
+Dir[File.join(__dir__, 'support', '**', '*.rb')].each { |f| require f }
